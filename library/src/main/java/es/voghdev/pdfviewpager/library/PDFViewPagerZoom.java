192842dbfc1db095be840d90940b71834e1a5592
Olmo Gallegos <vendofiatpunto@hotmail.com>
2016-07-27 22:43:42 +0200

    Added a scale parameter to attrs, added it to builder

--- library/src/main/java/es/voghdev/pdfviewpager/library/PDFViewPagerZoom.java
+++ library/src/main/java/es/voghdev/pdfviewpager/library/PDFViewPagerZoom.java
@@ -16,10 +16,12 @@
 package es.voghdev.pdfviewpager.library;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
 import es.voghdev.pdfviewpager.library.adapter.PDFPagerAdapter;
+import es.voghdev.pdfviewpager.library.adapter.PdfScale;
 
 public class PDFViewPagerZoom extends PDFViewPager {
     public PDFViewPagerZoom(Context context, String pdfPath) {
@@ -36,6 +38,29 @@ public class PDFViewPagerZoom extends PDFViewPager {
                 .setOffScreenSize(getOffscreenPageLimit())
                 .create()
         );
+    }
+
+    protected void init(AttributeSet attrs){
+        if(isInEditMode()) {
+            setBackgroundResource(R.drawable.flaticon_pdf_dummy);
+            return;
+        }
+
+        if(attrs != null){
+            TypedArray a;
+
+            a = context.obtainStyledAttributes(attrs, R.styleable.PDFViewPager);
+            String assetFileName = a.getString(R.styleable.PDFViewPager_assetFileName);
+            float scale = a.getFloat(R.styleable.PDFViewPager_scale, PdfScale.DEFAULT_SCALE);
+
+            if( assetFileName != null && assetFileName.length() > 0 ) {
+                setAdapter(new PDFPagerAdapter.Builder(context)
+                        .setPdfPath(assetFileName)
+                        .setScale(scale)
+                        .setOffScreenSize(getOffscreenPageLimit())
+                        .create());
+            }
+        }
     }
 
     /**
