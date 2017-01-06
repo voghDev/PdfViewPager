4d96a80b97a0db581ca3db051443fb0284b821ef
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-01-06 13:35:31 +0100

    pass checkstyle, disable the 2000+ lines check

--- sample/src/androidTest/java/es/voghdev/pdfviewpager/sample/Sample2Tests.java
+++ sample/src/androidTest/java/es/voghdev/pdfviewpager/sample/Sample2Tests.java
@@ -34,8 +34,6 @@ import es.voghdev.pdfviewpager.sample.idlingresource.WaitIdlingResource;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.swipeLeft;
-import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
